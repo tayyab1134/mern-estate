e6c41b3ace4f2cfd4da3c81ee105e28582b15b91
Tayyab <tayyabali50055@gmail.com>
2025-06-17 13:13:03 +0500

    complete signup UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,13 +9,13 @@ import Header from "./components/header";
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
