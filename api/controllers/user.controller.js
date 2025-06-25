318aac86e09119f0e6494257a3625e7ef0c264c6
Tayyab <tayyabali50055@gmail.com>
2025-06-25 09:08:56 +0500

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
-//import { errorHandler } from "../utils/error.js";
+import { errorHandler } from "../utils/error.js";
 
 export const test = (req, res) => {
   res.json({
@@ -32,6 +32,18 @@ export const updateUser = async (req, res, next) => {
     const { password, ...rest } = updatedUser._doc;
 
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete your own account!"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
   }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,36 +6,39 @@ import {
   getDownloadURL,
   uploadBytesResumable,
 } from "firebase/storage";
-import { updateUserStart, updateUserSuccess,updateUserFailure } from "../redux/user/userSlice";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+} from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import { app } from "../firebase";
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser ,loading , error} = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
-    const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
 
   const [formData, setFormData] = useState({});
-    const dispatch = useDispatch();
-
-  
-
-  
+  const dispatch = useDispatch();
 
   // firebase storage
   // allow read;
   // allow write: if
   // request.resource.size < 2 * 1024 * 1024 &&
   // request.resource.contentType.matches('image/.*')
-  
+
   useEffect(() => {
     if (file) {
       handleFileUpload(file);
     }
-  }, [file]);//file
+  }, [file]); //file
 
   const handleFileUpload = (file) => {
     const storage = getStorage(app);
@@ -50,7 +53,8 @@ export default function Profile() {
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setFilePerc(Math.round(progress));
       },
-      (error) => { //error
+      (error) => {
+        //error
         setFileUploadError(true);
       },
       () => {
@@ -64,14 +68,14 @@ export default function Profile() {
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
-const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       dispatch(updateUserStart());
       const res = await fetch(`/api/user/update/${currentUser._id}`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
@@ -88,6 +92,22 @@ const handleSubmit = async (e) => {
     }
   };
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -123,48 +143,47 @@ const handleSubmit = async (e) => {
         <input
           type="text"
           placeholder="username"
-                    defaultValue={currentUser.username}
-
+          defaultValue={currentUser.username}
           id="username"
           className="border p-3 rounded-lg"
-                    onChange={handleChange}
-
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="email"
-                    defaultValue={currentUser.email}
-
+          defaultValue={currentUser.email}
           id="email"
           className="border p-3 rounded-lg"
-                    onChange={handleChange}
-
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           id="password"
           className="border p-3 rounded-lg"
-                    onChange={handleChange}
-
+          onChange={handleChange}
         />
         <button
-                  disabled={loading}
-
-                 className=" bg-slate-700 text-white rounded-lg 
+          disabled={loading}
+          className=" bg-slate-700 text-white rounded-lg 
        p-3 uppercase hover:opacity-95 disabled:opacity-80"
         >
-                    {loading ? 'Loading...' : 'Update'}
-
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete account
+        </span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
-      <p className="text-red-700 mt-5">{error ? error:''}</p>
-      <p className="text-green-700 mt-5">{updateSuccess ? 'user update successfully!' : ''}</p>
-
+      <p className="text-red-700  mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5">
+        {updateSuccess ? "user update successfully!" : ""}
+      </p>
     </div>
   );
 }
