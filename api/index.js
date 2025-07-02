14457c0a69525e04d6de06c3e809cc6c405b2636
Tayyab <tayyabali50055@gmail.com>
2025-07-02 18:33:33 +0500

    fix server file

--- api/index.js
+++ api/index.js
@@ -20,9 +20,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
-});
+//app.listen(3000, () => {
+  //console.log("Server is running on port 3000!");
+//});
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
