f4097d3bde0d3fbc46ea97e4a9ded01e27b3e491
@Tayyab Ali <tayyabasif029@gmail.com>
2025-07-07 19:55:42 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,10 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: 'https://backend-mernestate.vercel.app',
+        target: "http://localhost:3000",
          changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
 
       },
     },
