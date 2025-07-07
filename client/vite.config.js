2d746fd0e53a2e8b361a1a9becbc8740329d834a
Tayyab <tayyabali50055@gmail.com>
2025-07-07 13:55:52 +0500

    add url backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,9 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: 'https://backend-mernestate.vercel.app',
          changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+
       },
     },
   },
