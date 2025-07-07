0791f02091240a9c47f9193d1e92d284e9f3747d
@Tayyab Ali <tayyabasif029@gmail.com>
2025-07-07 20:03:40 +0500

    Update CreateListing.jsx

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -182,7 +182,7 @@ export default function CreateListing() {
         return setError("Discount price must be lower than regular price");
       setLoading(true);
       setError(false);
-      const res = await fetch("https://backend-mernestate.vercel.app/api/listing/create", {
+      const res = await fetch("/api/listing/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
