4bbeb7c1806a1f709030e9713bc737be3d6e1349
Tayyab <tayyabali50055@gmail.com>
2025-07-09 13:29:57 +0500

    update all

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -25,7 +25,7 @@ export default function SignIn() {
     e.preventDefault();
     try {
       dispatch(signInStart());
-      const res = await fetch("https://backend-mernestate.vercel.app/api/auth/signin", {
+      const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://backend-mernestate.vercel.app",
+        target: "http://localhost:3000",
          changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+      
       },
     },
   },
